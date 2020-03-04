16ae026383ac7ff0af5aebf020a38da49a793930
Shaon H <shaon2u@gmail.com>
2020-03-05 01:05:16 +0800

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,33 @@
+let Name;
+console.log(name);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    result
+}
+const result = add(13, 82)
+console.log(result);
+
+problem
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+const result = add (13);
+console.log(result);
+
+solution
+function add(num1, num2=10) {
+    console.log(num1, num2);
+    return(num1 * num2)
+}
+const result = add(13);
+console.log(result);
+
+const developer = {name: "Jone", phone:1234621};
+console.log(developer.new);
+
+let fun = undefined;
+console.log(fun);
+
+let age = [2,5,9]
+console.log(age[11]);
